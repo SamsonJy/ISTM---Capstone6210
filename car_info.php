f70c44ea3c873975b73c3e834df2e5bd57facc88
stevezhao1 <99219516+stevezhao1@users.noreply.github.com>
2022-11-20 19:31:38 -0500

    car_info_update

--- car_info.php
+++ car_info.php
@@ -14,76 +14,38 @@ session_start();
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>My Vehicles</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>My Vehicles</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
 </head>
 
 <body>
-    <h1>Your car info</h1>
-    <?php foreach ($cars as $car) { ?>
+<?php   
+        include 'car_info_header.php'
+    ?>
 
-        <p> <?php echo $car["vehicle_id"]  ?>
-        <div>
-            <label style="font-weight: bold"><?php echo $car['plate_number']; ?>
-                -
-                <?php echo $car['state']; ?></label>
-            <br />
-            <label style="font-weight: bold">Brand & Model:
-                <?php echo $car['brand']; ?></label>
-            <br />
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#update" data-bs-whatever="@mdo">Update</button>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Delete</button>
+  <h1>Your car info</h1>
+  <hr>
+  <?php foreach ($cars as $car) { ?>
+
+
+    <div>
+      <label style="font-weight: bold"><?php echo $car['plate_number']; ?>
+        -
+        <?php echo $car['state']; ?></label>
+      <br />
+      <label style="font-weight: bold">Brand & Model:
+        <?php echo $car['brand']; ?></label>
+      <br />
+      <button type="button" class="btn btn-primary" onclick="location.href='car_info_update.php?id=<?php echo $car['vehicle_id'] ?>'">Update</button>
+      <button type="button" class="btn btn-danger" onclick="location.href='car_info_delete.php?id=<?php echo $car['vehicle_id'] ?>'">Delete</button>
+      <hr>
     <?php } ?>
+   
+    <button type="button" class="btn btn-light" onclick="location.href='car_info_add.php'">Add vehicle</button>
 
 
-    <div class="modal fade" id="update" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Update car info</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form action="car_info_update.php?id=" method="POST"> 
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-            <button type="submit" class="btn btn-primary">Send message</button>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-    const exampleModal = document.getElementById('update')
-exampleModal.addEventListener('show.bs.modal', event => {
-  // Button that triggered the modal
-  const button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  const recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  const modalTitle = exampleModal.querySelector('.modal-title')
-  const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-  modalTitle.textContent = `New message to ${recipient}`
-  modalBodyInput.value = recipient
-})
-</script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
