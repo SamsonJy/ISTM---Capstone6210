03d08d276e18d75e321379b0d657f7d82ae3c55a
stevezhao1 <99219516+stevezhao1@users.noreply.github.com>
2022-11-26 14:56:23 -0500

    remove echo message

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,5 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     
-}else{
-    echo "success";
 }
 mysqli_select_db ( $conn , $dbname);
