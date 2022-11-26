f1aaed77947d6751aa1692bef2c8c6868ee8c4b8
Haolin Song <72663301+ethanshl@users.noreply.github.com>
2022-11-26 16:44:44 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,4 +5,8 @@ $conn = mysqli_connect('localhost', 'ethan', '123456', 'GWVP');
 if(!$conn){
   echo "Conncetion error: " . mysqli_connect_error();
 }
+<<<<<<< HEAD
 ?>
+=======
+mysqli_select_db ( $conn , $dbname);
+>>>>>>> parent of 2b43e7e (add back php tag close)
