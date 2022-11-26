11686ffd6c573f757d0b943b51332c3d32d2ac40
stevezhao1 <99219516+stevezhao1@users.noreply.github.com>
2022-11-26 14:49:02 -0500

    connect database, card info update message

--- db_connect.php
+++ db_connect.php
@@ -8,5 +8,7 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     
+}else{
+    echo "success";
 }
 mysqli_select_db ( $conn , $dbname);
