9e11271eb5ee06831552e99b6c7aeb9847272af4
Haolin Song <72663301+ethanshl@users.noreply.github.com>
2022-11-25 19:36:29 -0500

    reservation page button list styling

--- js/javaScript.js
+++ js/javaScript.js
@@ -20,10 +20,3 @@ function checkDate() {
   }
 	return true;
 };
-function show(n) {
-    document.getElementById("table1").style.display="none";
-    document.getElementById("table2").style.display="none";
-    document.getElementById("table3").style.display="none";
-    document.getElementById("table" + t).style.display="block";
-};
-
