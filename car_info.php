7b96b0634875299f2117115cc56fca23418fc073
stevezhao1 <sichunzhao11@gmail.com>
2022-12-02 23:07:30 -0500

    Updated payment and vehicle info changing

--- car_info.php
+++ car_info.php
@@ -13,39 +13,66 @@ mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>My Vehicles</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-</head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body>
-<header>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="path/to/jquery-3.5.0.js"><\/script>')
+    </script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
+
+    <script src="js/javaScript.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Change Password</title>
+    <body>
+    <header>
         <div>
             <div class="container-fluid">
-                <div>
-                    <h3 class="display-4">GWU Parking System</h3>
+                <div style="float:right;margin:20px">
+                    <a style="margin-left:7px;font-weight:bold;color:#2a1484" href="utilities/logout.php">Log out</a>
                 </div>
+                <h3 class="display-4">GW Parking System</h3>
             </div>
-
-        </div>
     </header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" href="home.html">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link" href="contact.html">Contact Us</a>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Reservations</a>
+                </li>
+
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="car_info.php">My Vehicle</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="payment_info.php">My Payment</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="password.php">My Password</a>
+                </li>
+            </ul>
         </div>
     </nav>
-    <img src="images\BAS_Parking hero_1920x400.jpg" alt="GWU Garage" />
 
 
     <div class="pt-4">
