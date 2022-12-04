dd7483e0121d598b4d4606d513282d03e0293ecf
Xueliang Chen <51306962+cxlgo64@users.noreply.github.com>
2022-12-03 21:22:39 -0500

    cancel feature finished

--- js/javaScript.js
+++ js/javaScript.js
@@ -27,3 +27,36 @@ function show(n) {
     document.getElementById("table" + t).style.display="block";
 };
 
+function checkDate2() {
+  var dateString3 = document.getElementById("startDate2").value;
+  var dateString4 = document.getElementById("endDate2").value;
+	var dateStart2 = new Date(dateString3).getTime();
+  var dateEnd2 = new Date(dateString4).getTime();
+  var startTime2 = $('#startTime2').timepicker('getTime');
+  var endTime2 = $('#endTime2').timepicker('getTime');
+
+	if (dateEnd2 < dateStart2) {
+		alert("The end date cannot be earlier than the start date.");
+		document.getElementById('endDate2').value = "";
+		return false;
+  }
+  if(dateEnd2 == dateStart2) {
+    if(endTime2 <= startTime2) {
+      alert("The end time cannot be earlier than or equal to the start time.");
+      document.getElementById('endTime2').value = "";
+      return false;
+    }
+  }
+	return true;
+};
+
+function extendFunction(){
+  var form1 = document.getElementById("reservation_extend_form")
+  if(form1.style.display=="none"){
+    document.getElementById("reservation_extend_form").style.display="block";
+   
+  } else{
+    document.getElementById("reservation_extend_form").style.display="none";
+  }
+}
+
--- reservations.php
+++ reservations.php
@@ -3,6 +3,47 @@ include('db_connect.php' );
 session_start();
 //$userID = $_SESSION['userID'];
 $userID = 1;
+//cancel process
+if(isset($_POST["reservationId"])){
+  $_SEESION['reservationId'] = $_POST['reservationId'];
+  $ridc = $_SEESION['reservationId'];
+  $sqlCancel = "UPDATE `reservations` SET `reservation_status` = 'Cancelled' WHERE `reservations`.`reservation_id` = $ridc";
+  mysqli_query($conn,$sqlCancel);
+  $_POST["reservationId"]=null;
+  header("Refresh:0");
+}
+//extend process
+// if(isset($_POST["reservationId2"])){
+//   $ridE = $_POST['reservationId2'];
+//   $startDateString = $_POST['startDate2'];
+//   $endDateString = $_POST['startTime2'];
+//   $startTimeString = $_POST['endDate2'];
+//   $endTimeString = $_POST['endTime2'];
+
+//   $date1 = explode('-', $startDateString);
+//   $date2 = explode('-', $endDateString);
+//   $time1 = explode(':', $startTimeString);
+//   $time2 = explode(':', $endTimeString);
+
+//   $startDate = (int)$date1[0] . $date1[1]. $date1[2];
+//   $endDate = (int)$date2[0] . $date2[1] . $date2[2];
+//   $startTimeHour = (int)$time1[0];
+//   $endTimeHour = (int)$time2[0];
+//   $totalTimeHour = 0;
+//   $pDate = $endDate - $startDate;
+//   if ($endTimeHour >= $startTimeHour) {
+//     $pTime = $endTimeHour - $startTimeHour;
+//     $totalTimeHour = $pDate * 24 + $pTime;
+//   }else {
+//     $pTime = $startTimeHour - $endTimeHour;
+//     $totalTimeHour = $pDate * 24 - $pTime;
+//   }
+//   echo $totalTimeHour;
+//   $sqlextend = "UPDATE `reservations` SET `exit_date` = $_SEESION['endDate2'], `exit_time` = $_SEESION['endTime2'], `duration_in_hours`=$totalTimeHour WHERE `reservations`.`reservation_id` = $ridE";
+//   mysqli_query($conn,$sqlextend);
+//   $_POST["reservationId2"]=null;
+//   header("Refresh:0");
+// }
 //Get reservations, garages, payments and vehicle infos
 $sqlUpcoming = "SELECT * FROM reservations WHERE reservation_status = 'Upcoming' AND user_id = '$userID'";
 $upcomingResult = mysqli_query($conn, $sqlUpcoming);
@@ -12,7 +53,7 @@ $sqlOngoing = "SELECT * FROM reservations WHERE reservation_status = 'Ongoing' A
 $ongoingResult = mysqli_query($conn, $sqlOngoing);
 $ongoingReservations = mysqli_fetch_all($ongoingResult, MYSQLI_ASSOC);
 
-$sqlPast = "SELECT * FROM reservations WHERE reservation_status = 'Finished' AND user_id = '$userID' OR reservation_status = 'Canceled' AND user_id = '$userID'";
+$sqlPast = "SELECT * FROM reservations WHERE reservation_status = 'Finished' AND user_id = '$userID' OR reservation_status = 'Cancelled' AND user_id = '$userID'";
 $pastResult = mysqli_query($conn, $sqlPast);
 $pastReservations = mysqli_fetch_all($pastResult, MYSQLI_ASSOC);
 
@@ -37,6 +78,7 @@ mysqli_free_result($pastResult);
 mysqli_close($conn);
 ?>
 
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -50,15 +92,31 @@ mysqli_close($conn);
 		<script>window.jQuery || document.write('<script src="path/to/jquery-3.5.0.js"><\/script>')</script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 		<!-- Latest compiled JavaScript -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
 
 		<script src="js/javaScript.js"></script>
+    <script type="text/javascript" src="js/qrcode.js"></script>
 		<link rel="stylesheet" href="css/styles.css">
     <link href="../css/multiColumnTemplate.css" rel="stylesheet" type="text/css">
 		<title>Reservation Records</title>
+    <script> $(document).ready(function(){
+				$('input.timepicker').timepicker({
+					timeFormat: 'H:mm ',
+					interval: 60,
+					dynamic: false,
+					dropdown: true,
+					scrollbar: true,
+					minTime:'7:00am',
+					maxTime: '11:00pm',
+					change: checkDate2,
+          zindex: 9999999
+				});
+		});</script>
 	</head>
-
+  
 	<body>
 		<header>
 			<div>
@@ -186,43 +244,111 @@ mysqli_close($conn);
                   document.getElementById("table"+n).style.display="block";
       
               };
+              
               </script>
 
     <!-- Modal -->
     <div class="modal fade" id="detail-modal" tabindex="-1" role="dialog" aria-labelledby="DetailModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="DetailModalLabel">Reservation Details</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
           </div>
           <div class="modal-body">
-            <table class="reservation_details">
-              <thead>
-                <tr>
-                  <th scope="col">Garage Name</th>
-                  <th scope="col">Garage Location</th>
-                  <th scope="col">Start Time</th>
-                  <th scope="col">End Time</th>
-                  <th scope="col">Vehicle Model</th>
-                  <th scope="col">Vehicle Plate</th> 
-                  <th scope="col">Vehicle Color</th>
-                  <th scope="col">Payment Info</th>
-                </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td id="garageN"></td>
-                <td id="garageD"></td>
-                <td id="timeS"></td>
-                <td id="timeE"></td>
-                <td id="vehicleM"></td>
-                <td id="vehicleP"></td>
-                <td id="vehicleC"></td>
-                <td id="paymentNum"></td>
-              </tr>
-            </tbody>
-            </table>
+          <div class=" p-0 jumbotron " id="reservation_cancel_form"  >
+			  <div class="container-fluid">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" id="cancelForm" class="needs-validation" novalidate>
+              <div class="row">
+                <div class="form-group col"  style="display:none;">
+                <input type="text" id="reservationId" class="form-control" name="reservationId">
+                </div>
+                <div class="col-12 col-md-4">
+                  <label for="garageName">Garage Name:</label>
+                  <p id="garageN"></p>
+                </div>
+                <div class="col-6 col-md-4">
+                  <label for="garageLocation">Garage Location:</label>
+                  <p id="garageD"></p>
+                </div>
+            </div>
+              <div class="row">
+                <div class="col-12 col-md-4">
+                  <label for="timeS">Start Time:</label>
+                  <p id="timeS"></p>
+                </div>
+                <div class="col-6 col-md-4">
+                  <label for="timeE">End Time:</label>
+                  <p id="timeE"></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 col-md-4">
+                  <label for="vehicleM">Vehicle Model:</label>
+                  <p id="vehicleM"></p>
+                </div>
+                <div class="col-6 col-md-2">
+                  <label for="vehicleP">Vehicle Plate:</label>
+                  <p id="vehicleP"></p>
+                </div>
+                <div class="col-6 col-md-4">
+                  <label for="vehicleC">Vehicle Color:</label>
+                  <p id="vehicleC"></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                  <label for="garageLocation">Payment Info:</label>
+                  <p id="paymentNum"></p>
+                </div>
+              
+                <div class="col-6">
+                <label for="qrcode">QR code for Garage Access:</label>
+                <div id="qrcode"></div>
+                    <script type="text/javascript">
+                    new QRCode(document.getElementById("qrcode"), "For Test");
+                    </script>
+                </div>
+            </div>
+              
+            </form>
+            </div>
+					</div>
+            <div class=" p-0 jumbotron " id="reservation_extend_form" style="display:none" >
+			<div class="container">
+				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" id="extendForm" class="needs-validation" novalidate>
+
+					<div class="row">
+            <div class="form-group col"  style="display:none;">
+              <input type="text" id="reservationId2" class="form-control" name="reservationId2">
+            </div>
+					
+						<div class="form-group col">
+							<label for="endDate2">New Exit Time</label>
+							<input type="date" id="endDate2" min="2022-1-17" class="form-control" name="endDate2" required>
+							<div class="valid-feedback"></div>
+							<div class="invalid-feedback">
+								Please select departure date.
+							</div>
+						</div>  
+					</div>
+
+					<div class="row">
+						<div class="form-group col">
+							<input type="text" id="endTime2" class="form-control timepicker" name="endTime2" required>
+							<div class="valid-feedback"></div>
+							<div class="invalid-feedback">
+								Please select departure time.
+							</div>
+						</div>
+					</div>
+
+					<div class="homeButton">           
+            <button type="button" class="btn btn-primary" id="extendUpdate" onclick="extendSubmit()">Update</button>
+					</div>
+				</form>
+			</div>
+		</div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -242,8 +368,11 @@ mysqli_close($conn);
       var vehicles = $json_vehicle;
       </script>"?>
       <script>
+
+
           //functions for modal
         $('#detail-modal').on('show.bs.modal', function (event) {
+         
           //change the modal table use the data passed from data-whatever
           var button = $(event.relatedTarget) 
           var record = button.data('whatever') 
@@ -273,6 +402,16 @@ mysqli_close($conn);
           modal.find('.modal-body #vehicleP').text(vehicle['plate_number']+", "+ vehicle['state'])
           modal.find('.modal-body #vehicleC').text(vehicle['color'])
           modal.find('.modal-body #paymentNum').text(payment['card_number'])
+          modal.find('.modal-body #reservationId').val(record['reservation_id'])
+          modal.find('.modal-body #startTime2').val(record['arrival_time'])
+          modal.find('.modal-body #endTime2').val(record['exit_time'])
+          modal.find('.modal-body #startDate2').val(record['arrival_date'])
+          modal.find('.modal-body #endDate2').val(record['exit_time'])
+          modal.find('.modal-body #reservationId2').val(record['reservation_id'])
+        })
+
+        $('#detail-modal').on('hidden.bs.modal', function () {
+          document.getElementById("reservation_extend_form").style.display="none";
         })
       </script>
 
@@ -280,27 +419,14 @@ mysqli_close($conn);
         //function for cancal redirect
         function cancelFunction(){
           if(confirm("Do you want to cancel this order?")){
-              //$_SESSION['reservation_change'] = record['reservation_id'];
-              alert("Reservation cancal Sucessfully!");
-            <?php
-              include('db_connect.php' );
-              $sqlCancel = "UPDATE reservations SET reservation_status = 'Cancelled' WHERE reservation_id = 'rid'";
-              $upcomingResult = mysqli_query($conn, $sqlUpcoming);
-              mysqli_close($conn);
-            ?>
-
-             
-            location.reload();
-          } else {
-
-          }
-
+              alert("Reservation cancal Sucessfully!");     
+              document.getElementById("cancelForm").submit();
+          } 
         }
 
-        function extendFunction(){
-          if(confirm("Do you want to cancel this order?")){
-            location.href="extendPage.php";
-          }
+        function extendSubmit(){
+          alert("Your reservation is updated now!");                
+          document.getElementById("extendForm").submit();
         }
       </script>
     </div>
